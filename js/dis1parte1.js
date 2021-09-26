01de63ac3cc95c14223b497a8c361feec3106e3d
Wilfredo Acosta <wilfredo.acosta10@hotmail.com>
2021-09-26 15:19:01 -0600

    discusion 1 lista

---
+++ js/dis1parte1.js
@@ -0,0 +1,4 @@
+document.write("<header><h1>Header</h1></header>");
+document.write("<nav><h3>Nav</h3></nav>");
+document.write("<section><article><h2>Article</h2></article><article><h2>Article</h2></article><article><h2>Article</h2></article> </section>");
+document.write("<footer>Footer</footer>");
